5295196b58fadb06ff8b7fdeeb7b9ae23b46454f
Javier Koh <s10221847@connect.np.edu.sg>
2022-06-04 03:59:29 +0800

    testing repo

--- app/src/main/java/sg/edu/np/mad/mad_assignment_cookverse/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_assignment_cookverse/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.np.mad.mad_assignment_cookverse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView test=findViewById(R.id.test);
+        test.setText("Testing repo to get A+ :D");
+        TextView test2=findViewById(R.id.test2);
+        test2.setText("Hello ash ayrton kok kai and minh");
     }
 }
