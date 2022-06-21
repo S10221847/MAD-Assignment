c2c90cd3243ee67352e50866231908dfe0505a55
Tan-Kok-Kai <S10222863@connect.np.edu.sg>
2022-06-21 22:04:04 +0800

    Testing for set text in fragment

--- app/src/main/java/sg/edu/np/mad/mad_assignment_cookverse/DiscoverFragment.java
+++ app/src/main/java/sg/edu/np/mad/mad_assignment_cookverse/DiscoverFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -58,6 +59,10 @@ public class DiscoverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false);
+        View view = inflater.inflate(R.layout.fragment_discover, container, false);
+        TextView textView = view.findViewById(R.id.randomText);
+        textView.setText("Hello");
+        return view;
     }
+
 }
