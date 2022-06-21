f1823b4a40800f8bf6be40f91884fdc0f0efd2d5
Tan-Kok-Kai <S10222863@connect.np.edu.sg>
2022-06-21 22:05:03 +0800

    Testing for set text in fragment

--- app/build/generated/data_binding_base_class_source_out/debug/out/sg/edu/np/mad/mad_assignment_cookverse/databinding/FragmentDiscoverBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/sg/edu/np/mad/mad_assignment_cookverse/databinding/FragmentDiscoverBinding.java
@@ -5,19 +5,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 import sg.edu.np.mad.mad_assignment_cookverse.R;
 
 public final class FragmentDiscoverBinding implements ViewBinding {
   @NonNull
   private final FrameLayout rootView;
 
-  private FragmentDiscoverBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final TextView randomText;
+
+  private FragmentDiscoverBinding(@NonNull FrameLayout rootView, @NonNull TextView randomText) {
     this.rootView = rootView;
+    this.randomText = randomText;
   }
 
   @Override
@@ -43,10 +50,19 @@ public final class FragmentDiscoverBinding implements ViewBinding {
 
   @NonNull
   public static FragmentDiscoverBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.randomText;
+      TextView randomText = ViewBindings.findChildViewById(rootView, id);
+      if (randomText == null) {
+        break missingId;
+      }
 
-    return new FragmentDiscoverBinding((FrameLayout) rootView);
+      return new FragmentDiscoverBinding((FrameLayout) rootView, randomText);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
