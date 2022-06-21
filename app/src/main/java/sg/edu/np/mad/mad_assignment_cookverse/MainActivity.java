9c3539f331569f37d31edaa569b0fbe3005ebfc5
Huy Minh Tran <s10223485@connect.np.edu.sg>
2022-06-21 22:08:49 +0700

    Added Ash's login and sign up page, changed DB related method calls, added a couple of constraints to xml files

--- app/src/main/java/sg/edu/np/mad/mad_assignment_cookverse/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_assignment_cookverse/MainActivity.java
@@ -3,7 +3,13 @@ package sg.edu.np.mad.mad_assignment_cookverse;
 import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,60 +20,97 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public String TAG = "Main Activity";
+    public String MY_EMAIL_ADDRESS = "MyEmailAddress";
+    public String MY_PASSWORD = "MyPassword";
     DBHandler dbHandler = new DBHandler(this, null, null, 1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
 
-        try {
+        /*try {
             createInitialDatabase();
         } catch (JSONException e) {
             e.printStackTrace();
-        }
+        }*/
 
-        setContentView(R.layout.activity_main);
-        TextView test=findViewById(R.id.test);
-        test.setText("Testing repo to get A+ :D");
-        TextView test2=findViewById(R.id.test2);
-        test2.setText("Hello ash ayrton kok kai and minh");
-        //ayrton testing
+        TextView newUser = findViewById(R.id.userSignup);
+        newUser.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Intent myNewCreate = new Intent(MainActivity.this, createUserPage.class);
+                startActivity(myNewCreate);
+                return false;
+            }
+        });
+        Button myLoginButton = findViewById(R.id.myLoginButton);
+        myLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editMyEmailAddress = findViewById(R.id.editmyEmailAdd);
+                EditText editMyPassword = findViewById(R.id.editmyPassword);
+                try {
+                    if (isValidCredentials(editMyEmailAddress.getText().toString(), editMyPassword.getText().toString()) == true) {
+                        Intent myIntent = new Intent(MainActivity.this, HomeFragment.class);
+                        startActivity(myIntent);
+                        Toast.makeText(MainActivity.this, "Valid Credentials", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Invalid Credentials, try again.", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 
-    public void createInitialDatabase() throws JSONException {
-        Recipe recipeData = dbHandler.findRecipe(1); //it is only logical that the first recipe should be in the database
-
-        if(recipeData == null){
-            recipeData = new Recipe(); //1st Recipe, should automatically be assigned incremented primary key
-            recipeData.setName("Farro Salad with Asparagus and Parmesan");
-            recipeData.setImage('1');
-            recipeData.setNoOfLikes(0);
-            recipeData.setDescription("A light and delicious way to get your whole grains. Perfect salad for picnics, baby or bridal showers or just because!\n" +
-                    "prep: 20 mins \n" +
-                    "\n" +
-                    "cook: 55 mins \n" +
-                    "\n" +
-                    "additional: 12 hrs \n" +
-                    "\n" +
-                    "total: 13 hrs 15 mins \n" +
-                    "\n" +
-                    "Servings: 12 \n" +
-                    "\n" +
-                    "Yield: 12 servings \n" +
-                    "\n" +
-                    "Ingredients: 2 cups Farro, ¾ pound fresh asparagus trimmed, 1 cup red and yellow cherry tomatoes halved, ¾ cup chopped walnuts, ¾ cup dried cranberries, ½ cup chopped fresh parsley, 1/3 cup chopped fresh chives, ¼ cup balsamic vinaigrette or to taste, 1 cup shaved Parmesan cheese divided\n" +
-                    "\n" +
-                    "\n" +
-                    "Step 1: Soak farro in a large bowl of water for at least 12 hours. Drain.\n" +
-                    "Step 2: Fill a large pot with lightly salted water and bring to a rolling boil over high heat. Once the water is boiling, stir in the drained farro, and return to a boil. Reduce heat to medium, then cook the farro uncovered, stirring occasionally for 20 minutes. Reduce heat to low, cover, and continue simmering until tender, about 30 more minutes. Drain and allow to cool.\n" +
-                    "Step 3: Bring a large pot of lightly salted water to a boil. Add the asparagus, and cook uncovered until tender, about 3 minutes. Drain in a colander, then immediately immerse in ice water for several minutes until cold to stop the cooking process. Once the asparagus is cold, drain well, and chop. Set aside.\n" +
-                    "Step 4: Place farro, asparagus, tomatoes, walnuts, cranberries, parsley, and chives in a large bowl. Drizzle the balsamic vinaigrette over and sprinkle about 3/4 cups Parmesan cheese, then toss. Top with the remaining 1/4 cup of Parmesan cheese. Serve at room temperature.\n");
-            ArrayList<String> iList = new ArrayList<String>(Arrays.asList("Farro", "Asparagus", "Red and Yellow Cherry Tomatoes", "Walnuts", "Cranberries", "Parsley", "Chives", "Balsamic Vinaigrette", "Parmesan Cheese"));
-            recipeData.setIngredientList(iList);
-            ArrayList<String> cList = new ArrayList<String>(Arrays.asList("Greek","Italian"));
-            recipeData.setCuisineList(cList);
-            //recipeData.setUserId(); only use when recipe is created by User, otherwise should automatically be null
-            dbHandler.addRecipe(recipeData);
+    public Boolean isValidCredentials(String email_address, String password) throws JSONException {
+        User userDBData = dbHandler.findUserByName(email_address);
+        if (userDBData == null) {
+            Toast.makeText(MainActivity.this, "User Does Not Exist!", Toast.LENGTH_SHORT).show();
+        } else {
+            if (userDBData.getName().equals(email_address) && userDBData.getPassword().equals(password)) {
+                return true;
+            }
         }
+        return false;
+    }
+    /*ImageView myImage = findViewById(R.id.imageView);*/
+
+    public void createInitialDatabase() throws JSONException {
+
+        Recipe recipeData = new Recipe(); //1st Recipe, should automatically be assigned incremented primary key
+        recipeData.setName("Farro Salad with Asparagus and Parmesan");
+        recipeData.setImage('1');
+        recipeData.setNoOfLikes(0);
+        recipeData.setDescription("A light and delicious way to get your whole grains. Perfect salad for picnics, baby or bridal showers or just because!\n" +
+                "prep: 20 mins \n" +
+                "\n" +
+                "cook: 55 mins \n" +
+                "\n" +
+                "additional: 12 hrs \n" +
+                "\n" +
+                "total: 13 hrs 15 mins \n" +
+                "\n" +
+                "Servings: 12 \n" +
+                "\n" +
+                "Yield: 12 servings \n" +
+                "\n" +
+                "Ingredients: 2 cups Farro, ¾ pound fresh asparagus trimmed, 1 cup red and yellow cherry tomatoes halved, ¾ cup chopped walnuts, ¾ cup dried cranberries, ½ cup chopped fresh parsley, 1/3 cup chopped fresh chives, ¼ cup balsamic vinaigrette or to taste, 1 cup shaved Parmesan cheese divided\n" +
+                "\n" +
+                "\n" +
+                "Step 1: Soak farro in a large bowl of water for at least 12 hours. Drain.\n" +
+                "Step 2: Fill a large pot with lightly salted water and bring to a rolling boil over high heat. Once the water is boiling, stir in the drained farro, and return to a boil. Reduce heat to medium, then cook the farro uncovered, stirring occasionally for 20 minutes. Reduce heat to low, cover, and continue simmering until tender, about 30 more minutes. Drain and allow to cool.\n" +
+                "Step 3: Bring a large pot of lightly salted water to a boil. Add the asparagus, and cook uncovered until tender, about 3 minutes. Drain in a colander, then immediately immerse in ice water for several minutes until cold to stop the cooking process. Once the asparagus is cold, drain well, and chop. Set aside.\n" +
+                "Step 4: Place farro, asparagus, tomatoes, walnuts, cranberries, parsley, and chives in a large bowl. Drizzle the balsamic vinaigrette over and sprinkle about 3/4 cups Parmesan cheese, then toss. Top with the remaining 1/4 cup of Parmesan cheese. Serve at room temperature.\n");
+        ArrayList<String> iList = new ArrayList<String>(Arrays.asList("Farro", "Asparagus", "Red and Yellow Cherry Tomatoes", "Walnuts", "Cranberries", "Parsley", "Chives", "Balsamic Vinaigrette", "Parmesan Cheese"));
+        recipeData.setIngredientList(iList);
+        ArrayList<String> cList = new ArrayList<String>(Arrays.asList("Greek", "Italian"));
+        recipeData.setCuisineList(cList);
+        //recipeData.setUserId(); only use when recipe is created by User, otherwise should automatically be null
+        dbHandler.addRecipe(recipeData);
     }
 }
---
+++ app/src/main/java/sg/edu/np/mad/mad_assignment_cookverse/createUserPage.java
@@ -0,0 +1,70 @@
+package sg.edu.np.mad.mad_assignment_cookverse;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+
+public class createUserPage extends AppCompatActivity {
+    public String TAG = "Create User Page";
+    public String MY_EMAIL_ADDRESS = "MyEmailAddress";
+    public String MY_PASSWORD = "MyPassword";
+    DBHandler dbHandler = new DBHandler(this,null,null,1);
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creater_user_page);
+
+        EditText myCreateEmailAddress = findViewById(R.id.editmyEmailAdd);
+        EditText myCreatePassword = findViewById(R.id.editmyPassword);
+
+        Button myButtonCreate = findViewById(R.id.buttonCreate);
+        myButtonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User userDBData = null;
+                try {
+                    userDBData = dbHandler.findUserByName(myCreateEmailAddress.getText().toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if (userDBData == null) {
+                    User userDataDB = new User();
+                    userDataDB.setName(myCreateEmailAddress.getText().toString());
+                    userDataDB.setPassword((myCreatePassword.getText().toString()));
+                    userDataDB.setLikedList(null);
+                    userDataDB.setCreatedList(null);
+                    try {
+                        dbHandler.addUser(userDataDB);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(createUserPage.this, "Account Created Successfully.", Toast.LENGTH_SHORT).show();
+
+                    Intent myCreateIntent = new Intent(createUserPage.this, MainActivity.class);
+                    startActivity(myCreateIntent);
+                }
+                else {
+                    Toast.makeText(createUserPage.this, "User Already Exists! Try again!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        Button myButtonCancel = findViewById(R.id.buttonCancel);
+        myButtonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(createUserPage.this,"Creation Cancelled",Toast.LENGTH_SHORT).show();
+                Intent myCancelIntent = new Intent(createUserPage.this,MainActivity.class);
+                startActivity(myCancelIntent);
+            }
+        });
+
+    }
+}
+
